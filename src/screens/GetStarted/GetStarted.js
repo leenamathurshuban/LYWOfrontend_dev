d7979b61cfdc5118c7cbfc3e723b2910845c2cf7
Shivani Latya <leena.mathur@shubansoftware.com>
2024-11-18 12:09:19 +0530

    get started update screen

--- src/screens/GetStarted/GetStarted.js
+++ src/screens/GetStarted/GetStarted.js
@@ -11,16 +11,16 @@ const Getstarted = () => {
     <div className='authinfo_slider text-center'>
         <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <h2>Revolutionize<br></br> Your Hiring Process with<br></br> Behavioral Insights</h2>
-        <p>Transform hiring with LYWO's AI-powered personality assessments, guaranteeing the perfect fit for every role.</p>
+        <h2>Discover the Power of <br></br> Personality-Based Hiring</h2>
+        <p>Unlock the potential of your workforce with LYWO’s easy-to-use, yet highly effective & scientific personality-based hiring. Find candidates that truly enjoy the work and thereby ensure a productive workplace.</p>
       </Carousel.Item>
       <Carousel.Item>
-        <h2>Revolutionize<br></br> Your Hiring Process with<br></br> Behavioral Insights</h2>
-        <p>Transform hiring with LYWO's AI-powered personality assessments, guaranteeing the perfect fit for every role.</p>
+        <h2>Eliminate Guesswork and <br></br> Embrace Precision</h2>
+        <p>Our transparent and structured hiring process ensures crystal-clear, informed decisions, putting everything in your control. Experience truly customized hiring with LYWO’s dual analysis of roles and candidates creating perfect professional matches.</p>
       </Carousel.Item>
       <Carousel.Item>
-        <h2>Revolutionize<br></br> Your Hiring Process with<br></br> Behavioral Insights</h2>
-        <p>Transform hiring with LYWO's AI-powered personality assessments, guaranteeing the perfect fit for every role.</p>
+        <h2>Streamline Hiring with <br></br> Behavioral Insights</h2>
+        <p>LYWO’s advanced tools help you swiftly identify the best candidates and streamline your hiring process. With LYWO, you can quickly sort through thousands of applications and make the most informed decisions faster.</p>
       </Carousel.Item>
     </Carousel>
     </div>
