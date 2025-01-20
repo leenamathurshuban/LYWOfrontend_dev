aea92396b3ac801d30b71ee5e80b4724019c5aa2
Shivani Latya <leena.mathur@shubansoftware.com>
2025-01-20 11:19:45 +0530

    routes add with id

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -66,8 +66,8 @@ export const router = createBrowserRouter([
     element: <ProtectedRoute element={<JobsList />} allowAuthenticated={false} />,  // Only allow authenticated users here
   },
   {
-    // path: "/JobPosts/:id",
-    path: "/JobPosts",
+    path: "/jobposts/:id",
+    // path: "/JobPosts",
     element: <ProtectedRoute element={<JobPosts />} allowAuthenticated={false} />,  // Only allow authenticated users here
   },
   {
